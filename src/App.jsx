d724a8a4751da3f6c7125520fe90ee18ca17caaf
isaquebatista <isaquebatista1716@gmail.com>
2023-04-18 17:54:17 -0300

    Eventos, listando itens com o map, corrigindo erro de key prop.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
 import React from "react";
 
 function App() {
+  const lista = [
+    { id: 123456789, task: "Estudar" },
+    { id: 987654321, task: "Candidatar nas vagas" },
+    {
+      id: 1234598765,
+      task: "Conversar e fechar com várias pessoas na Amakha Paris",
+    },
+  ];
+  // "Estudar", "Candidatar nas vagas", "Conversar e fechar com várias pessoas na Amakha Paris"
+  function digitarInput(e) {
+    console.log(e.target.value);
+  }
+  function buttonClick() {
+    console.log("buttin click ok");
+  }
+
   return (
     <div>
-      <h1>Hello world</h1>
+      <input onChange={digitarInput} placeholder="O que tenho que fazer..." />
+      <button onClick={buttonClick}>Adicionar</button>
+
+      <ul>
+        {lista.map((item) => (
+          <li>{item.task}</li>
+        ))}
+      </ul>
     </div>
   );
 }
